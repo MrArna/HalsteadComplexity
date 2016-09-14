738559f7d9453948644af9efb5d66a5b525ba731
Marco Arnaboldi <mr.arnaboldi@gmail.com>
2016-09-14 09:23:42 -0500

    First commit in the fork

---
+++ src/main/resources/test.java
@@ -0,0 +1,10 @@
+import java.util.ArrayList;
+ 
+public class Apple {
+	public static void main(String[] args) {
+		ArrayList<String> al = null;
+		int j =0;
+		System.out.println(j);
+		System.out.println(al);
+	}
+}
